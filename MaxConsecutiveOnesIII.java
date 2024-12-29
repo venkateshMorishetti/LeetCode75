75d4d7595b24fcb403331632fdba18deae80ad7d
venkateshMorishetti <117025754+venkateshMorishetti@users.noreply.github.com>
2024-12-29 16:07:14 +0530

    1004. Max Consecutive Ones III

---
+++ MaxConsecutiveOnesIII.java
@@ -0,0 +1,43 @@
+/**
+    Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+    Example 1:
+
+    Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+    Output: 6
+    Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+    Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+    Example 2:
+
+    Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+    Output: 10
+    Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+    Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+ */
+
+
+ class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0, right = 0, num_zeros = 0, result = 0;
+
+        while(right<nums.length){
+            if(nums[right] == 0){
+                num_zeros++;
+            }
+
+            while(num_zeros > k){
+                if(nums[left] == 0){
+                    num_zeros--;
+                }
+                left++;
+            }
+            int w = right-left+1;
+            if(result < w){
+                result = w;
+            }
+            right++;
+        }
+        return result;
+
+    }
+}
